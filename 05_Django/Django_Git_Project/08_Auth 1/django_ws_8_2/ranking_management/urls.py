df445dd7ae896f9c804df9636742fe6813e40172
kdhn97 <ehdgus3726@naver.com>
2024-04-01 17:48:44 +0900

    0401

--- 05_Django/Django_Git_Project/08_Auth 1/django_ws_8_2/ranking_management/urls.py
+++ 05_Django/Django_Git_Project/08_Auth 1/django_ws_8_2/ranking_management/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for crud project.
+URL configuration for ranking_management project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
 ]
