073e2113a1e86385ca94d5dbf64858be3f912422
kdhn97 <ehdgus3726@naver.com>
2024-05-13 14:46:59 +0900

    0513

---
+++ 08_Vue/Vue_Git_Project/08_Vue with DRF 1/vue_hw_8_2/front/src/components/ArticleList.vue
@@ -0,0 +1,27 @@
+<template>
+    <button>게시글 생성</button>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue';
+import { useArticleStore } from '@/stores/articles'
+const store = useArticleStore()
+
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: `http://127.0.0.1:8000/api/v1/articles/`,
+  })
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 08_Vue/Vue_Git_Project/08_Vue with DRF 1/vue_hw_8_4/front/src/views/CreateView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>게시글 작성</h1>
+    <form @submit.prevent="createArticle">
+      <input type="text" v-model.trim="title">
+      <textarea v-model.trim="content"></textarea>
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
+import { useRouter } from 'vue-router'
+
+const store = useCounterStore()
+const title = ref(null)
+const content = ref(null)
+const router = useRouter()
+
+const createArticle = function () {
+  axios({
+    method: 'post',
+    url: `${store.API_URL}/api/v1/`,
+    data: {
+      title: title.value,
+      content: content.value
+    }
+  })
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+</script>
+
+<style>
+
+</style>
